185af09faff6e0e51bdf723760919fcecd0a3bd7
Marko Lenkkeri <marko.lenkkeri@geniem.com>
2017-03-12 10:21:37 +0200

    removed debug from xml stuff, fixed iterating over data

--- grails-app/services/fi/lenkkeri/airline/grails/XmlApiService.groovy
+++ grails-app/services/fi/lenkkeri/airline/grails/XmlApiService.groovy
@@ -18,46 +18,40 @@ class XmlApiService {
     //man!
     List<Pilot> findPilot(String gfirstName, String glastName) {    //man!
         List<Pilot> foundPilots = []    //man!
-        println "Searching for $gfirstName and $glastName"
-        def uploadableData = ["firstName":gfirstName, "lastName":glastName]    //man!
-        def writer = new StringWriter()
-        def xmld = new MarkupBuilder(writer)
-        xmld.FindPersonCommand() {
-            firstName(gfirstName)
-            lastName(glastName)
-        }
-        uploadableData = writer.toString()
+        println "Searching for $gfirstName and $glastName"    //man!
 
         def http = new HTTPBuilder( grailsApplication.config.getProperty("fi.lenkkeri.grails.xml.baseurl").toString()+"findPerson" )    //man!
 
         http.handler.failure = { resp ->    //man!
-            log.error "Response: $resp"
+            log.error "Response: $resp"    //man!
             log.error "Unexpected failure: ${resp.statusLine}"    //man!
         }
 
-        http.request( POST, groovyx.net.http.ContentType.XML ) { req ->    //man!
-            requestContentType : groovyx.net.http.ContentType.XML
-            body = uploadableData //man!
+        http.request( POST ) { req ->    //man!
+            requestContentType = groovyx.net.http.ContentType.XML
+            body = {    //man!
+                FindPersonCommand {    //man!
+                    firstName gfirstName    //man!
+                    lastName glastName    //man!
+                }
+            }
             response.success = { resp, xml ->    //man!
                 if(resp.statusLine.statusCode==200)    //man!
                 {
-                    println convertToMap(xml)
-                    xml?.each { pilotXml ->   //man!
-                        def pilotMap = convertToMap(pilotXml)?.entry?.pilotData
+                    xml?.entry?.find {it.@key=='data'}?.pilotData?.each { pilotXml ->   //man!
+                        def pilotMap = convertToMap(pilotXml)    //man!
                         Pilot pilot = [firstName:pilotMap?.firstName, lastName:pilotMap?.lastName, middleName:pilotMap?.middleName, birthDate:Date.parse("yyyy-MM-dd HH:mm:ss.S", pilotMap?.birthDate), enteredEmployment:Date.parse("yyyy-MM-dd HH:mm:ss.S", pilotMap?.enteredEmployment), callsign:pilotMap?.callsign]   //man!
-                        println "Pilot:" + pilot //TODO: poista kun testattu
                         foundPilots.add(pilot)  //man!
                     }
                 }
             }
-
         }
         return foundPilots    //man!
     }
 
-    def convertToMap(nodes) {
-        nodes.children().collectEntries {
-            [ it.name(), it.childNodes() ? convertToMap(it) : it.text() ]
+    def convertToMap(nodes) {    //man!
+        nodes.children().collectEntries {    //man!
+            [ it.name(), it.childNodes() ? convertToMap(it) : it.text() ]    //man!
         }
     }
 }
